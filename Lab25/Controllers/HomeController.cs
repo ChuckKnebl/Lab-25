651d1f2668b75a75ab9e903bdebab2f3d447e3d5
Chuck Knebl <chuck.knebl@att.net>
2017-05-03 00:35:12 -0400

    Commit for Lab 25

---
+++ Lab25/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fish()
+        {
+            ViewBag.Message = "Everything you've ever wanted to know about fish.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
